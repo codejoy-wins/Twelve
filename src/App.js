b1fb854f8dea18983106c34472a6c0c45bbe4299
Max <mjann@ucsc.edu>
2019-05-30 16:39:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,421 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>12 Articles</h1>
+      <div id="image1"></div>
+      <h2>Cloudland</h2>
+      <p>Once upon a time, in a land in the clouds, a princess was locked in a castle.
+
+
+
+      Our hero, Elam, disembarked on a quest to save her.
+      
+      
+      
+      But she was guarded by 6 dragons.
+      
+      
+      
+      Msixes, Msicar, Aibohpomoh, and Ytinilucsamcixot swirled around the castle breathing fire.
+      
+      
+      
+      These dragons were the fiercest. and have devoured everyone who tried to save the Princess of the clouds.
+      
+      
+      
+      One dragon, Noitanimircsid, was the most intelligent. Noitanimircsid believed that the best defense was a good offense. This dragon operated in search and destroy mode, always devouring a new target in flames. Assuring no hero could even make it to the castle.
+      
+      
+      
+      The last Dragon was called Ecnarelot. And this dragon was so dangerous, so cunning, that people prayed to him each night. It is said in cloudland, that if you do not worship Ecnarelot, he will swallow you whole in your dreams and you will never wake up.
+      
+      
+      
+      Cloudland was not always this way. Only a decade ago, Elam Ahpla fell in love with the princess, before the Dragons had taken her away, and she loved him back. 
+      
+      
+      
+      Elam was 20 years old when she was taken from him by the Dragon, Ecnarelot in the night.
+      
+      
+      
+      For 7 years Elam and Cloudland were constantly under attack from Noitanimircsid.
+      
+      
+      
+      Until one day, Elam decided he was willing to risk his life to save the princess's. He stopped praying to Ecnarelot.
+      
+      
+      
+      At 27, he picked up his father's sword and crafted his own shield and began his quest to save her. 
+      
+      
+      
+      
+      
+      The path was long, winding, and confusing, but he persited.
+      
+      
+      
+      The path was narrow, perilous, and infused with darkness, but he perservered. 
+      
+      
+      
+      The path ahead was opaquely covered in dark clouds, but he pushed forward.
+      
+      
+      
+      The path was slippery and cursed, but he dared not turn back.
+      
+      
+      
+      Until finally, the castle could be seen in the distance, with several dragons swirling around it more and more fiercely as he got closer until...
+      
+      
+      
+      "Cease, at once, elam. It is I, Ytinilucsamcixot. I cannot let you pass."
+      
+      
+      
+      Elam staggered backward, but remembered that there was nothing back in cloudland for him unless he could bring back the princess with him. 
+      
+      
+      
+      He barked back, "I do not fear you, Ytinilucsamcixot , for you do not exist, but are rather a shade of existance," He threw his sword at the dragon with extreme speed.
+      
+      
+      
+      The dragon was hit by this, and retreated to the castle, hissing.
+      
+      
+      
+      
+      
+      Now Elam continued his trek, and could feel the castle getting closer, and the dragons swirling faster.
+      
+      
+      
+      Until suddenly, Msixes snuck up on him from behind.  
+      
+      
+      
+      "Elam, why do you come here?"
+      
+      
+      
+      "To save my princess." He replied.
+      
+      
+      
+      "She does not need to be saved by the likes of you," hissed the dragon. "If she wanted to leave, she can do that herself."
+      
+      
+      
+      "Then let me ask her myself." Elam spat back, readying his shield.
+      
+      
+      
+      Msixes spat a fireball at our hero, but our hero was not afraid. He deflected the fireball with his shield and as the dragon swirled around the path, Elam leapt off the narrow road, and landed on the dragon's back.
+      
+      
+      
+      Next, Elam pulled out a dagger and stabbed the dragon in the top of the neck, to which it shrieked and rolled Elam off him, but retreated to the castle.
+      
+      
+      
+      Elam fell and barely managed to catch himself on the edge of the narrow road, dangling. 
+      
+      
+      
+      
+      
+      
+      
+      Whilst dangling, yet another dragon came forth, this time with a booming voice.
+      
+      
+      
+      "Obey me" boomed Msicar.
+      
+      
+      
+      "Never."
+      
+      
+      
+      "Obedience is pleasure, resistance is pain" , said Msicar.
+      
+      
+      
+      "No, pleasure is me saving the princess and taking her back to cloudland where she belongs.", replied Elam, our hero.
+      
+      
+      
+      At this point Msicar summoned several demons with his eyes, and they all spawned ahead of Elam on the narrow road.
+      
+      
+      
+      
+      
+      Elam had no sword because he had thrown it at Ytinilucsamcixot. No dagger because he had stabbed it into the neck of Msixes. 
+      
+      
+      
+      But he still had his shield, so he deflected the incoming demonic hand to hand attacks. The demons were dark and short, with red eyes, and Elam shield bashed them off the edge of the narrow road, one by one until Msicar was furious and began breathing fire on the entire path.
+      
+      
+      
+      Elam jumped on top of the last demons head to avoid the flames, leapt into the air spinning 540 degrees and throwing his shield like a frisbee into the dragon's throat, where he choked and died right there, falling out of the sky. 
+      
+      
+      
+      Elam could hear cheers from deep below in cloudland, or maybe it was just his imagination. 
+      
+      
+      
+      He looked toward the castle, still distant, but much closer now. The sky changed colors to a dark red with thunder and lightning. The wounded dragons swirled faster and faster. 
+      
+      
+      
+      Aibohpomoh, an unharmed dragon, decided to bring another dragon with him after seeing Msicar fall so he telepathically summoned Noitanimircsid, the most offensive and most intelligent dragon, to attack Elam with him.
+      
+      
+      
+      
+      
+      
+      
+      Elam had no shield, no sword, and no dagger, so he looked around for a hiding spot. There was none to be seen, and he could see the dragons gathering closer. So, he looked over the edge, underneath the path and noticed rungs.
+      
+      
+      
+      He transfered himself under the narrow road and swung himself from rung to rung, eyes fixed on the castle. 
+      
+      
+      
+      
+      
+      The two dragons could not see him because of the harsh weather they themselves had summoned. and they began attacking eachother in confusion. Both the dragons bit each others necks at the same time, and fell.
+      
+      
+      
+      
+      
+      Elam, seeing them fall, swung himself back onto the path and continued forward. 3 down, 3 to go. 2 of the remaing 3 dragons were wounded. But a wounded dragon's fire is twice as hot.
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      Ecnarelot suddenly spoke to Elam telepathically from the castle,
+      
+      
+      
+      "What do you seek?"
+      
+      
+      
+      "My princess".
+      
+      
+      
+      "Put on a gown, and call yourself princess, and you will have what you desire."
+      
+      
+      
+      "No, what I desire is both me as I am, and her as she is."
+      
+      
+      
+      
+      
+      " She doesn't want you"... hissed the dragon into Elam's mind.
+      
+      
+      
+      " I have to hear it from her." - returned Elam, and started sprinting toward the castle now. 
+      
+      
+      
+      The castle door was shot so Elam began scaling the building with dragons swirling around him. He was knocked off by the wounded Msixes, and managed to catch himself by the handle of the sword sticking out of Ytinilucsamcixot he had put there earlier.
+      
+      
+      
+      
+      
+      He pulled it out, and the beast died and dropped to a thousand depths below as Elam leapt off and began scaling the building with his sword remounted to his back. 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      Msixes made another pass at Elam but Elam cut his eyes out as he was flying by, and the beast fled the castle to the horizon. As he left, Elam, smiled at the beautiful sight of a dragon flying into the sunset.  
+      
+      
+      
+      
+      
+      
+      
+      He called out, "Princess!! "
+      
+      
+      
+      
+      
+      She popped out the window and looked beautiful, dressed in a silk pink gown, glimmering with dragon scales that bordered on transparency.
+      
+      
+      
+      
+      
+      
+      
+      At this point, Ecnarelot appeared. An absolute behemoth of a dragon. Larger than the other 5 combined.
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      "TURN BACK NOW."
+      
+      
+      
+      "No." said Elam.
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      " ACCEPT THINGS AS THEY ARE, EVEN IF YOU DO NOT LIKE IT "
+      
+      
+      
+      "As long as I have breath, I will not. " said Elam.
+      
+      
+      
+      
+      
+      "THEN I WILL TAKE YOUR BREATH" hissed Ecnarelot.
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      Suddenly Elam Ahpla couldn't breath, and he dropped his sword and gasped for air. 
+      
+      
+      
+      The Princess, moved by his journey, jumped out the window and onto the dragons back. He was so Massive and powerful he did not feel her walk with such grace.
+      
+      
+      
+      
+      
+      She reached Elam, but he wasn't breathing, so she put her mouth onto his and breathed her air into him. 
+      
+      
+      
+      
+      
+      
+      
+      Elam awoke, sprung up, and dipped her like in a dance and began kissing her. The Dragon was furious. The sky turned dark purple. Ecnarelot turned completely black in hue, like a shadow. 
+      
+      
+      
+      
+      
+      "I cast you aside, Ecnarelot." said Elam, "The princess is with me now."
+      
+      
+      
+      
+      
+      And he began spinning around with the princess, like a dance. 
+      
+      
+      
+      Together they swung his sword at the dragon with amplified force. The dragon was hit, fled sourly, and Ecnarelot was no more. 
+      
+      
+      
+      Elam and his princess walked back to Cloudland, as the sound of cheering grew louder. 
+      
+      
+      
+      
+      
+      THE END
+      
+      </p>
+      <div id="image2"></div>
+      <h2>Gender Pay Gap Philosophy</h2>
+      <p>I'm going to attempt to clarify my position on why I defend the gender pay gap.
+
+      I don't think it is the government's role to tell businesses what to pay their employees.
+      
+      If it is true that female workers are underpaid compared to their male counterparts in a specific domain, I believe that business would lose to a business that paid them fairly.
+      
+      For example, if you have the male engineers make 100K a year and only pay the women 65K for the same exact quality of work, then here's what I think would be able to happen.
+      
+      A new software company could spring up and take advantage of underpaid good engineers. They could offer the underpaid females higher money, say 90K. Now you have two companies, both equally skilled engineers. You have one company paying 100K per employee (all men) and another company hiring only women at 90K. Since there is no difference between their skills, the 90K business could afford to sell their products for cheaper, or pay more for advertisements and R&D.
+      
+      That is what I believe would start to happen if you actually had underpaid people because of arbitrary reasons like gender.
+      
+      Okay, so let's assume that people get paid what they are worth for the sake of argument. That means women are not as valuable workers to the company if you break down the statistics for this hypothetical company.
+      
+      Why might this be? How to explain that without asking the government to save you, and how to explain that without making gender stereotypes. We live in an over-sensitized world where you're calling me sexist. I'm not sexist. I'm making gross generalizations about potential explanations for the gender gap other than sexism. I think you are falsely attributing sexism as the cause of the gap, just as you are falsely accusing me of sexism now.
+      
+      I believe a better explanation than companies overpaying males is that males, for various reasons, tend to value money more and work harder for it.
+      
+      Let me give you a scientifically proven example. Some little boys like dolls and some little girls like hot wheels, but on average, boys like things like cars more than girls, whereas girls care more about people than inanimate objects. This puts us at a huge evolutionary advantage if we work together with the masculine and feminine.
+      
+      A gender pay gap has 2 potential explanations.
+      
+      a) The employers are sexist
+      
+      b) Genders provide different values
+      
+      So either you're calling someone a sexist, or you're the sexist. Right? No, it's not sexist to say genders provide different value at work. That would only be sexist if you assume both genders are trying equally. If they're both trying equally, but men are paid more, and the employers aren't sexist, that implies one gender is better than the other at the job, and that's what's got everyone so hot and bothered.
+      
+      In my opinion, the best explanation I can think of is that different genders have different values and therefore the performance reflects that. Women inherently have the gift of interpersonal relationships, and they may value spending time with the family, or having a healthy work-life balance more so than men, on average, thus explaining the difference in value produced at work. It doesn't have to be a competence issue. We can just call it a difference in priorities and therefore effort.
+      
+      In conclusion, let's please stop using sexism to talk about this issue. There shouldn't be anything derogatory about embracing the reality of traditional gender roles that are evolutionarily programmed into our reward systems. I don't care about equality of outcome; what I care about is equality of opportunity. If a gender is marginalized in their opportunity then I will listen to you, but I'm not in support of the government mandating I pay genders equally. I'd rather pay them what they're worth and let the chips fall where they may. Whatever happened to Martin Luther King, Jr? Let's not pay people by the their gender, color, or anything other than the content of their work. We can attempt to analyze data that way in search for answers, but when it comes to actual policy, don't tell me what to pay my employees, and don't call me sexist.</p>
+    <h2>Another One</h2>
+    <p>Lorem </p>
+    <h2>Last One</h2>
+    <p>Lorem more</p>
     </div>
   );
 }
