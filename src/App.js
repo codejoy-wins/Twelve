ca7642ee138968717b14524178e2bf3cf65aee01
Max <mjann@ucsc.edu>
2019-05-30 17:22:43 -0700

    almost

--- src/App.js
+++ src/App.js
@@ -567,7 +567,191 @@ function App() {
     So there is no way to find out what x is, without y. There is no way to find out what y is, without x. Even if you know r. The x's and y's form the circle. Us trying to find the position of an electron is like trying to find x without y. The y position changes when we choose a specific x. The x position changes when we choose a specific y. Therefore, it can be in multiple places, but the moment we measure where it is, based on the other axis, it shifts into one place. Before that, it was in circular form, like the wave form of the narrow slit experiment in quantum physics. So maybe the electron is in another dimension, circularly, and the moment we measure where it is, based on this dimension, it shifts into being, but when it is not measured against this dimension, it is in multiple places at once. So maybe when a tree falls in the forest with no one around it doesn't make a noise here, but it does in any other dimensions where there are observations. It only makes the noise in the dimensions against which it is measured. If you do not give an x coordinate for a circle, you will not find the y coordinate of the circle. You will only find a range of possibilities based on the radius. That explains the double-slit experiment's results..</p>
 
 
+    <div id="image8"></div>
+    <h2>Infoverse</h2>
+    <p> Sometimes I think about how I would invent a forcefield. I do this not because I think I can, but as a way to get my thought process into a productive state. I concluded that in order to build the type of device I had in mind, it would be information based. It would be information trickery, or hacking of reality.
+
+    When you walk into a room and do not fall through the floor it is because of what your feet are contacting. Your feet only touch part of the floor. On those points of contact, the information of the entire floor is contained and passed to your body, holding you up. Eventually I think we will be able to hijack this informational exchange and lie to the body. The body will be able to levitate as if there is a floor, even though there is not, simply by getting the wrong information.
     
+    If you step off a cliff, the body doesn't have a reason to stay in the air. It will fall. But you don't need a floor to step on. You only need the message of the floor. After all, You stand up just by contacting part of the floor, and your body figures out the rest based on the feedback from that contact point. So imagine instead of the floor, just boots with floor strapped below them with a computer slapped on telling your body that you are getting the message that there is a floor there. That would be anti-gravity boots. See, trying to invent these things dealing with classical physics doesn't work, so you have to attack physics rather than gravity. We can use classical physics to our advantage by lying about it. Even if gravity is real, somewhere in the process of your feet holding you up there is information exchange and we need to manipulate that to generate a true force field.
+    
+    I was just researching retrocausality and quantum physics and they said atoms move incrementally, as if there is a frame-rate on a computer. So there's no such thing as movement, just snaps of frames. Life is like a movie.
+    
+    I've never met anyone who understands my forcefield theory and maybe it doesn't make any sense but it certainly is a fun thought exercise that makes me feel worthwhile to my own self. I just really think that the universe might be only information, and if it's information it can be manipulated. Infoverse. Infoworld. Our body is a receiver and producer of signals that govern our environment.
+    
+    I imagine riding on my invisible hoverboard that basically skates around the sky and has a neural connection to my brain so wherever I want a ramp to appear, that's where the board reacts to. So I broadcast the signal of where I want the ramp to be, and then my board lies to itself about reality and instead uses my signal of what i want reality to be to create a ramp like effect in the sky.
+    
+    So anyway, a bullet is flying at the forcefield and the forcefield would broadcast to the bullet an impenetrable wall signal, and the bullet would drop the same way it would if it hit an impenetrable wall. When a bullet hits a wall it falls when it's told to. So we need to tell it to fall. The whole fact that you can make everything with atoms I think lends itself to eventually having the ability to create anything from anything. How do we informationally lie? Does it become true? Would the wall show up? or could we broadcast the function of the wall without the appearance of a wall?
+    
+    My main contention is that when things react physically, and obey the laws of physics, they are actually receiving a signal to do so before doing so. So there is room for altering that signal artificially. When I get punched in the face hard, the fist carries different information with it than if I was punched gently. It's the same fist but different information from the fist of what's behind it. Maybe it's just pure force that changes the impact of the fist, and I'll have to study more physics if I'm going to keep talking about it. But maybe force is just an integer, passed along as information into the function of punch.</p>
+    <code>more code I can't post here yet</code>
+    <p>Experimentally, this would start out in a cage with a bird. Can we zap the bird without any electricity? Can we get to the point of manipulating the birds input of reality so that it thinks it has been shocked? If we can do that without really shocking it, then we can stop a bullet without anything touching it. If we can lie to the bird, we can lie to the bullet. Or maybe not, maybe it's easier to manipulate the consciousness of a multicellular organism than a speeding bullet but I think the same principles will apply to all things. The bullet and the bird are both game objects, subject to informational exchanges.
+
+    The End.</p>
+
+    <div id="image9"></div>
+    <h2>Understanding Logarithms</h2>
+    <p>Saying, "Log this of that equals n" is the same as saying, "This to the nth power equals that".
+
+    If 10^4 = 10000 then log(10000) =4
+    
+    Log unspecified has an implied base 10.
+    
+    If 10^4 = 10000 then log base 10 of 10000 = 4
+    
+    
+    
+    So if exponents are asking, "if you multiply yourself by yourself this many times what will you get?", then logs are asking, " How many times did you multiply the base of this log by to get this number?"
+    
+    
+    
+    If 2^3 = 8 then log base 2 of 8 = 3
+    
+    if you want it in base ten you can use the change of base formula:
+    
+    log base x of y = (log base 10 of y) / (log base 10 of x)
+    
+    so log base 2 of 8 = (log base 10 of 8) / (log base 10 of 2)
+    
+    aka (log(8)) / (log(2)) = 3
+    
+    
+    
+    so if I asked you what log base 4 of 16 was how would you rephrase it?How many times did you multiply 4 by itself to get 16? 2 times
+    
+    So log base 4 of 16 = 2
+    
+    
+    
+    Well, really you multiplied it by itself once. So really logs are asking, " What is the exponent of this base that would give me this number?"So log base 3 of 81 = "What exponent of this base equals this number?" or more specifically, "What exponent of 3 equals 81?" And the answer is... well, 3^2 = 9 and 9^2 = 81 . so (3*3)  * (3*3) has 4 3's so I'm gonna conclude 3^4 = 81, which means that log base 3 of 81 = 4. Log's are asking for the exponent.  This is useful for algebra problems where the variable is in the exponent. Like algorithm notation.
+    
+    I have been studying Algebra 2 and Big O (Big Omicron) notation at the same time. They are connected. A binary search tree's efficiency and scalability are measured as 0(log n) or something like that because a binary search of 8 numbers can take at most 3 guesses? Ya after the 3rd guess it has narrowed it down to just 2 numbers at most of the original 8 and received a response if it is right or not. Then, before the 4th guess, it can know with 100% certainty what the number is.
+    
+    Think of a number between 1 and 8. Got it? Okay... I choose 5. Higher or lower? Lets say you say it is higher, then I eliminate 1 through 5 as options and have 6,7,8 and choose 7, lets say you say lower, now I'm down to 6, and I guess it right. if it was lower then 5 I eliminate 6,7,8 and have 1,2,3,4,5 and choose 3, if it's lower or higher then 3 that leaves me with 2 options for my 3rd guess, then you say yes or no, and I don't have to guess a 4th time. I know it after 3 guesses with feedback. 2^3 = 8 and log base 2 of 8 = 3. So the idea is that, if you're playing 21 questions. You should be able to tell them they can pick any number between 1 and 2^21 and still figure it out, rather than an inefficient algorithm where you can only ask them to pick a number between 1 and 21.
+    
+    2^5 = 32. so after 5 questions I know your number if it's between 1 and 21. Even in the worst case scenario. Whereas, in the worst case scenario of an inefficient algorithm, It would take me 21 questions to get to your number between 1-21, if it was 21. This is tricky though because the array has to be sorted, and you have have to tell me if it's higher or lower. But without getting bogged down by the details, the essential point is that I can save asking 16 extra questions just by using a better approach.
+    
+    so the big o notation of asking, "Is it 1? Is it 2? ..." is big 0 (n) which is inferior to
+    
+    big 0(log n) because 10000>log(10000) because log(10000) = 4 so 10000>4 and less is more in computer science so 4 is better than 10000.
+    
+    
+    
+    p.s. after much deliberation, I have narrowed it down into one statement of understanding.
+    
+    logb10 (n) = y === 10^y = n
+    
+    
+    
+    log(n) = y === 10^y = n
+    
+    
+    
+    log(5) = x === 10^x = 5
+    
+    
+    
+    log(1000) = x === 10^x = 1000
+    
+    
+    
+    x == 3
+    
+    
+    
+    log this of that = number == this^number = that
+    
+    Here is the ultimate statement of understanding logarithms:
+    
+    Saying, "Log this of that equals n" is the same as saying, "This to the nth power = that".
+    
+    
+    
+    Edit: I have a new understanding:
+    
+    Let's say an algebra problem is 7^x = 1017
+    
+    7^x = 1017
+    
+    we need logs to solve this algebraically.
+    
+    We want to isolate the x.
+    
+    Imagine a log comes from the left hand side and hit's the 7^ and rolls it to the other side of the equation before settling in. Now the left side of the equation is just x because it fell down when the 7 was no longer holding it up.
+    
+    x = log base 7(1017)
+    
+    Let's verify this method with a trivial example.
+    
+    10^x = 1000
+    
+    x should equal 3
+    
+    a log comes rolling from the left and knocks the 10 with it, settling on the right side
+    
+    x = log base 10(1000)
+    
+    x = log(1000)
+    
+    log(1000) = 3
+    
+    So it works.</p>
+
+    <div id="image10"></div>
+    <h2>Hi Google</h2>
+    <p><code>can't </code></p>
+
+    <div id="image11"></div>
+    <h2>Thresholds</h2>
+    <p>thresh·old
+
+    ˈTHreSHˌ(h)ōld/
+    
+    noun
+    
+    
+    
+    a strip of wood, metal, or stone forming the bottom of a doorway and crossed in entering a house or room.
+    the magnitude or intensity that must be exceeded for a certain reaction, phenomenon, result, or condition to occur or be manifested.
+    Nothing is more interesting to me than Thresholds and Placebos (and fractals). We'll save placebos (and fractals) for another day. These two (three) ideas I believe are fundamental to understanding the subtleties of the universe.
+    
+    Why are thresholds important?
+    
+    My definition of a threshold is when a bee is buzzing and making a noise that exists but you cannot hear it until it comes close enough to you to detect it, and the threshold is the point at which you can. I think it's interesting that the dictionary suggests a threshold is something that must be passed before entering a house. I'm sure you've heard of the "foot in the door phenomenon". In the professional world, people tell you to get your foot in the door, but they leave out the part about the threshold crossing that must happen first.
+    
+    Thresholds are fascinating to me because it shows the vitality of life. How many things are happening to you right now, in secret? Just out of range of your perception. You have a million forces acting on your right now. I just whispered, "Hello everyone reading this". Did you hear it? What if I shout? Would you hear that? Did a fraction of that sound wave hit a butterfly who will land on you tomorrow? I don't know how sound waves travel, but this is merely an example of a threshold.
+    
+    Thresholds are fascinating. Imagine you have a castle wall. It can withstand 100 damage. Let us also say it has a threshold of 100 damage. Any singular attack of less than 100 damage will not be felt. if you smash it with 99 damage it will still have 100 health because you did not hit the threshold. It didn't even notice you were there. But it's still technically happening. Now, if a butterfly flies by and deals 1 damage at exactly the same time that you deal 99 damage, the castle falls. Without the butterfly, all of the strength of the battering ram wouldn't have even made a difference. If the butterfly hit the castle wall one second after the battering ram, it wouldn't have fallen.
+    
+    Let's compare this to a castle without a threshold, which would fall on the second battering ram, with or without the butterfly. Which scenario is more like life?
+    
+    It gets even deeper. Your conscious mind is a threshold. There are beliefs in your head that exist that you are unaware of. They hit a threshold and then become part of your conscious awareness, seemingly suddenly, like the butterfly knocking down the castle.
+    
+    A better example is a shield. A forcefield. A forcefield that could take 500 damage at a time no problem. No cumulative damage over time, just a threshold for when it breaks. a 300 damage attack and a 200 damage attack yield exactly the same result. This is life! This is trying to break into the video game industry, or getting a job as a computer programmer. As a warrior, you could do 200 damage to the forcefield and not break it. Then you train for a year. You deal 300 damage to the forcefield and it still won't break even though 200+300=500. You come back later and do 440 damage. Still nothing. No measurable results yet. (unless you find a better way to measure your progress, ding ding ding) . Maybe after doing 499 damage in one move and the forcefield still not falling, at this point this warrior attacker job seeker gets discouraged and considers the forcefield game industry impenetrable. Little did he know, he was only a butterfly away from destroying it. So my advice is keep improving and keep fighting. Measure yourself vs. yourself yesterday. You never know when the forcefield will break.
+    
+    Clip from screenshot: https://youtu.be/81s9Ek9jAgg
+    
+    P.S. Imagine you have an instrument that can detect absolutely everything. Every sound and light and whatever else. If you can't do that imagine a directional microphone able to pick up conversations from far away you can't hear with the naked ear. That means that that conversation was there for you to hear the whole time, you just didn't have the tools. My question is, if everything is happening to us all the time, then do our bodies have hidden knowledge of the whole universe? If the directional mic can pick up the sound, then it is hitting our bodies, though we cannot hear it. So, we are all having the exact same experience, with different thresholds. Thresholds of conscious reality. What if the reason every action has an equal and opposite reaction (which I never understand why no one has attempted to explain, not simply prove) What if the reason for that is that everything contains everything else. So me hitting you is me+everything else vs. you+everything else. Since you are contained in my everything else, and I am contained in your everything else, that equation equals out.
+    
+    I hit u = (I + (universe - I)) hit (u + (universe-u)) == universe hit universe.
+    
+    (Add on): There's a line in Superman where lex luther says, "some people can read war and peace and come away thinking it's a simple adventure story, others can look at the ingredients of a chewing gum wrapper and unlock the secrets of the universe." That's what I meant by our bodies contain knowledge of the universe, and it's not just our bodies, it's anything. Just by looking a one rock in space and that's all you have is one rock. You can determine from that one rock that there does not exist an ever-reaching-rock-destroying substance within this universe. I know for a fact something about the whole universe just by looking at one object because the object is proof that there does not exist it's object-killer. So in a sense, you can learn about the whole universe in any given thing. That's why life is more fair than you think. Although we all live under different circumstances, if we're paying attention, the secrets of the universe are right in front of your face. You don't have to be a monk to be enlightened. You just have to pay attention and respect the knowledge contained in everything you experienced. Going back to your body and even your mind, your body has all sorts of wisdom contained in it and sculpted by evolution. You see a snake and get scared because your body and mind are smarter than you know. You don't have to be taught to think a girl is pretty or a snake is scary. Your body and mind do that for you, and are built upon past knowledge.
+    
+    I guess this is what happens when you major in philosophy.</p>
+
+    <div id="image12"></div>
+    <h2>Godlike</h2>
+    <p>Starting a new project in a game engine is like starting a new universe as God. I decide where the floor goes, if there is one at all. I decide what players there are and what physics bind them.
+
+    Throughout my quest to learn programming I have had this divine sensation often. When you are outside of an enclosed system and in charge of everything that happens inside how can you not feel like a god? I can make things disappear, rewrite the laws of physics, all within my created system.
+    
+    Meanwhile, simulation theory has been gaining in its popularity, and I must say, the world does seem to be programmed or at least programmable.
+    
+    Take the A.I. for an enemy in a game I make in the future. When that A.I. reaches it's point, will it realize it is trapped in an enclosed system, of which I am god? Will human beings ever be smart enough to realize that we are trapped in God's game?
+    
+    And who is the real designer of my game? Is it me, or does credit go to God? What if we are in a simulation, and the credit should pass through me, through the game designer we call god, and infinitely regress until the one true God. To me, God is the end of infinite regression. The solution to the paradox. The Alpha and the Omega.
+    
+    But when I boot up unity in 3d, I feel more godlike than when I am doing any other activity. In honour of this godlike feeling I get from designing games, I will name my company "Godlike Games" or something of the sort. Farewell, loyal readers. I highly encourage you to download Unity for free and experience the godlike feeling.</p>
 
 
     </div>
